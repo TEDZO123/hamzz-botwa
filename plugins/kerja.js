b32e193256e4884b9d9e35c4537f8d9fe9fd19d3
MitsuriBotz <96665638+JulianKastara09@users.noreply.github.com>
2022-01-19 18:44:58 +0700

    Update kerja.js

--- plugins/kerja.js
+++ plugins/kerja.js
@@ -13,7 +13,7 @@ conn.mining = conn.mining ? conn.mining : {}
    delete conn.mining[m.chat]
   }, 5 * 1000)
 }
-handler.help = ['kerja']
+handler.help = ['ngecit']
 handler.tags = ['xp']
 handler.command = /^kerja|nguli/i
 handler.owner = false
